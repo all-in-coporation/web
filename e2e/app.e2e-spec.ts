a23626b571a3cbbcde5aec0bd3bdb1b16edc0d81
dmerniestic1987 <dmernies@uade.edu.ar>
2019-09-03 21:52:53 -0300

    fixed unit test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('allinnight-web App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('ALL IN NIGHT');
   });
 });
