5db4b932041745755aa25446d919b240be1319be
dmernies <dmernies@atixlabs.com>
2019-09-07 20:47:09 -0300

    server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-// default Heroku PORT
+// default Heroku PORT.
 app.listen(process.env.PORT || 3000);
